7153320efe112b29cf459918997e433d456e8783
Mitarani Bisoyi <makwizdev4@gmail.com>
2017-08-19 17:40:00 +0530

    Issue #8 GST description in PO Items fixed Version = 0.1.3

--- aec_erpnext/hooks_call/purchase_order.py
+++ aec_erpnext/hooks_call/purchase_order.py
@@ -4,39 +4,31 @@ from frappe import msgprint, _
 
 @frappe.whitelist()
 def populate_item_tax(self, method):
-	tax_type_1 = []
-	tax = ''
 	
 	if self.taxes_and_charges == "SGST & CGST":		
-		for d in self.get("items"):
+		for d in self.get("items"):			
+			gst_tax = ''
+			tax_sgst = ''
+			tax_cgst = ''
 			tax_type_sgst = frappe.db.sql("""\
 				select substring_index(tax_type, '-', 1) from `tabItem Tax` where parent = %s and tax_type like %s""",(d.item_code, 'SGST%'))
-
-			if tax_type_sgst[0][0]:
-				tax_type_1.append(tax_type_sgst[0][0])
-				tax = ''.join(tax_type_1)
-				frappe.db.set(self, d.makwiz_item_tax_rate, tax)
-			tax_type_sgst = ''
-
+			if tax_type_sgst:
+				tax_sgst = tax_type_sgst[0][0]
 			tax_type_cgst = frappe.db.sql("""\
 				select substring_index(tax_type, '-', 1) from `tabItem Tax` where parent = %s and tax_type like %s""",(d.item_code, 'CGST%'))
-			if tax_type_cgst:				
-				tax_type_1.append(tax_type_cgst[0][0])
-				tax = ''.join(tax_type_1)
-				frappe.db.set(self, d.makwiz_item_tax_rate, tax)
-			tax_type_cgst = ''
-
-			#d.makwiz_item_tax_rate = tax_type_1
-			
+			if tax_type_cgst:
+				tax_cgst = tax_type_cgst[0][0]
+			if tax_sgst and tax_cgst:
+				gst_tax = '('+tax_sgst+','+tax_cgst+')'			
+			d.makwiz_item_tax_rate = gst_tax		
 
 	if self.taxes_and_charges == "IGST":		
-		for d in self.get("items"):
+		for d in self.get("items"):			
+			gst_tax = ''
+			tax_igst = ''	
 			tax_type_igst = frappe.db.sql("""\
 				select substring_index(tax_type, '-', 1) from `tabItem Tax` where parent = %s and tax_type like %s""",(d.item_code, 'IGST%'))			
-			if tax_type_igst:				
-				tax_type_1.append(tax_type_igst[0][0])
-				tax = ''.join(tax_type_1)
-				frappe.db.set(self, d.makwiz_item_tax_rate, tax)
-			tax_type_igst = ''
-	
-	#d.makwiz_item_tax_rate = ''.join(tax_type_1)
+			if tax_type_igst:
+				tax_igst = tax_type_igst[0][0]			
+				gst_tax = '('+tax_igst+')'
+			d.makwiz_item_tax_rate = gst_tax
