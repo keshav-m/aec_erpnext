3e9d44615ade2e5b3e0cf240308f3498a2dc97d7
Manoj Kumar <makwizdev1@gmail.com>
2017-12-11 23:50:00 +0530

    1.3.0.1

--- aec_erpnext/aec/doctype/payment_request_voucher/payment_request_voucher.js
+++ aec_erpnext/aec/doctype/payment_request_voucher/payment_request_voucher.js
@@ -3,6 +3,36 @@
 
 frappe.ui.form.on('Payment Request Voucher', {
 	refresh: function(frm) {
+		
 
-	}
+	},
+payment_term:function(frm){	
+	return frappe.call({
+		doc: frm.doc,
+		method: "calculate_amount",		
+		callback: function(r) {
+			if(!r.exc) frm.refresh_fields("po_amount_in_figures");
+		}
+	})
+},
+
+po_no:function(frm){
+	return frappe.call({
+		doc: frm.doc,
+		method: "reset_fiels",		
+		callback: function(r) {
+			if(!r.exc) 
+				frm.refresh_fields("name_of_payee");
+				frm.refresh_fields("amount_in_figures");
+				frm.refresh_fields("po_amount_in_figures");
+				frm.refresh_fields("payment_term");
+				frm.refresh_fields("payment_terms");
+				frm.refresh_fields("po_taxes");
+				frm.refresh_fields("tax");
+				frm.refresh_fields("in_words");
+				
+		}
+	})
+}
+
 });
--- aec_erpnext/aec/doctype/payment_request_voucher/payment_request_voucher.py
+++ aec_erpnext/aec/doctype/payment_request_voucher/payment_request_voucher.py
@@ -4,7 +4,35 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import flt, cstr, cint
 from frappe.model.document import Document
 
 class PaymentRequestVoucher(Document):
-	pass
+	def validate(self):
+		self.set_amount_in_words()
+
+	def calculate_amount(self):
+		if (flt(self.po_amount_in_figures) !=0 and flt(self.payment_term) !=0):
+			if (flt(self.po_amount_in_figures) != 0):
+				self.amount_in_figures = (flt(self.po_amount_in_figures) * flt(self.payment_term))/100
+
+		if (flt(self.po_taxes) != 0):
+			self.tax = flt(self.po_taxes) 
+
+
+	
+	def set_amount_in_words(self):
+		if (flt(self.amount_in_figures) != 0):
+			from frappe.utils import money_in_words
+			self.in_words = money_in_words(self.amount_in_figures)
+	
+	def reset_fiels(self):
+		self.name_of_payee = ''
+		self.amount_in_figures = ''
+		self.po_amount_in_figures =''
+		self.payment_term = ''
+		self.payment_terms = ''
+		self.po_taxes = ''
+		self.tax = ''
+		self.in_words = ''
+
