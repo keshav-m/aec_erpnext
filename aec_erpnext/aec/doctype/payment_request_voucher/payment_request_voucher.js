8addbe0cfc7fa41f30a16b5271427d4b225e07de
Manoj Kumar <manoj.jmp@hotmail.com>
2018-08-15 17:24:26 +0530

    Reset issue of Payment Request Vouche

--- aec_erpnext/aec/doctype/payment_request_voucher/payment_request_voucher.js
+++ aec_erpnext/aec/doctype/payment_request_voucher/payment_request_voucher.js
@@ -14,25 +14,25 @@ payment_term:function(frm){
 			if(!r.exc) frm.refresh_fields("po_amount_in_figures");
 		}
 	})
-},
-
-po_no:function(frm){
-	return frappe.call({
-		doc: frm.doc,
-		method: "reset_fiels",		
-		callback: function(r) {
-			if(!r.exc) 
-				frm.refresh_fields("name_of_payee");
-				frm.refresh_fields("amount_in_figures");
-				frm.refresh_fields("po_amount_in_figures");
-				frm.refresh_fields("payment_term");
-				frm.refresh_fields("payment_terms");
-				frm.refresh_fields("po_taxes");
-				frm.refresh_fields("tax");
-				frm.refresh_fields("in_words");
-				
-		}
-	})
 }
+
+//po_no:function(frm){
+//	return frappe.call({
+//		doc: frm.doc,
+//		method: "reset_fiels",		
+//		callback: function(r) {
+//			if(!r.exc) 
+//				frm.refresh_fields("name_of_payee");
+//				frm.refresh_fields("amount_in_figures");
+//				frm.refresh_fields("po_amount_in_figures");
+//				frm.refresh_fields("payment_term");
+//				frm.refresh_fields("payment_terms");
+//				frm.refresh_fields("po_taxes");
+//				frm.refresh_fields("tax");
+//				frm.refresh_fields("in_words");
+//				
+//		}
+//	})
+//}
 
 });
